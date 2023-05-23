5795032a1d443c2a32cb2666fa6153ad14a8f9c1
jangdu <jjd0324@gmail.com>
2023-05-23 11:16:06 +0900

    init blog-backend project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+app.use(express.static(path.join(__dirname, "../blog/build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../blog/build/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../blog/build/index.html"));
+});
